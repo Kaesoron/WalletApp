ae0ac76102128afc40c23c01b56e4e7cdd661c57
Kaesoron <postal0055@gmail.com>
2025-05-16 13:47:02 +0300

    Dto

---
+++ src/main/java/org/kaesoron/wallet/dto/WalletOperationRequest.java
@@ -0,0 +1,25 @@
+package org.kaesoron.wallet.dto;
+
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.kaesoron.wallet.service.OperationType;
+
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class WalletOperationRequest {
+
+    @NotNull
+    private UUID walletId;
+
+    @NotNull
+    private OperationType operationType;
+
+    @Positive
+    private long amount;
+}
---
+++ src/main/java/org/kaesoron/wallet/service/OperationType.java
@@ -0,0 +1,6 @@
+package org.kaesoron.wallet.service;
+
+public enum OperationType {
+    DEPOSIT,
+    WITHDRAW
+}
