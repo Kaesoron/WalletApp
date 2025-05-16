ed1fa3519e907fe06e980cda9f47de90b87391d4
Kaesoron <postal0055@gmail.com>
2025-05-16 13:44:46 +0300

    Repository

---
+++ src/main/java/org/kaesoron/wallet/repository/WalletRepository.java
@@ -0,0 +1,19 @@
+package org.kaesoron.wallet.repository;
+
+import jakarta.persistence.LockModeType;
+import org.kaesoron.wallet.entity.Wallet;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface WalletRepository extends JpaRepository<Wallet, UUID> {
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("SELECT w FROM Wallet w WHERE w.id = :id")
+    Optional<Wallet> findByIdForUpdate(UUID id);
+}
